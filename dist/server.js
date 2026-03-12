c274a065357d50dd265fc9b3cb8379a5ca25dd71
t3chie <t3chie01@proton.me>
2026-03-13 00:21:31 +0530

    setup

---
+++ dist/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { config } from "dotenv";
+config();
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.get("/health", (req, res) => {
+    res.json({ success: "ok" });
+});
+app.get("/", () => { });
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost/${PORT}`);
+});
+//# sourceMappingURL=server.js.map
