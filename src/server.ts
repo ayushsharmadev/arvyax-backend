1371d6f7d8caddd2eabed2efaefdc04c67a648a7
ayushsharmadev <ayush.sharma.dev01@gmail.com>
2026-03-14 13:32:08 +0530

    final prod v1.6

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,13 @@ import JournalRoutes from "./routes/route.journal";
 import cors from "cors";
 config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 const app = express();
 
 const corsOptions = {
-  origin: process.env.FRONTEND_URL
-}
+  origin: process.env.FRONTEND_URL,
+};
 app.use(cors(corsOptions));
 app.use(express.json());
 
@@ -20,6 +20,6 @@ app.get("/health", (req, res) => {
 
 app.use("/api/journal", JournalRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
